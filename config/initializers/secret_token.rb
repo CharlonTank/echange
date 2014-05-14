59b7e8176463c24340d80e057af0658cd21bb2bb
Poilon <poilon@gmail.com>
2014-05-14 22:02:49 +0200

    init of the project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Echange::Application.config.secret_token = '53588a77c41aff3e00514f33b8920e75d62c966f9cefd87a0e0d87aeddcbca8f087414479be424f89d17d8063bd26d2b92e38122502fa77f72cfb78dfaf4dfcc'
